7992ad07accd63f746711b320ce8f524966c6c69
IgorMushk <127196231+IgorMushk@users.noreply.github.com>
2024-01-07 21:45:35 +0200

    Create dir redux / store.js & currencySlice.js - test store - baseCurrency - fix error react-redux (/store)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,44 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { LayoutHome } from "./LayoutHome";
+import HomePage from "pages/HomePage/HomePage";
+import RatesPage from "pages/RatesPage/RatesPage";
+import { useEffect } from "react";
+import { getCurrentPosition } from "service/getCurrentPosition";
+
 export const App = () => {
+
+  useEffect(()=> {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+    
+    function success(pos) {
+      const crd = pos.coords;
+      getCurrentPosition(crd)
+      
+      // console.log("Your current position is:");
+      // console.log(`Latitude : ${crd.latitude}`);
+      // console.log(`Longitude: ${crd.longitude}`);
+      // console.log(`More or less ${crd.accuracy} meters.`);
+    }
+    
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+    
+    navigator.geolocation.getCurrentPosition(success, error, options);
+    
+  }, [])
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template!
-    </div>
+    <Routes>
+      <Route path="/" element={<LayoutHome/>}> 
+        <Route index element={<HomePage/>}/>
+        <Route path="rates" element={<RatesPage/>}/>
+      </Route>
+      <Route path="*" element={<Navigate to={"/"}/>} />
+    </Routes>
   );
 };
